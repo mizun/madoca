a97ddb4abf08200865807d4650e6a548bac9da57
mizun <hanamizutarezou@gmail.com>
2011-07-24 13:02:23 +0900

    bg constructing is cancelled

--- public/javascripts/show-samp.js
+++ public/javascripts/show-samp.js
@@ -1,5 +1,6 @@
   function show_sample(){
-    generate_plate();
+//    generate_plate();
+//		arrange_plate();
     set_player_cards();
     select_player();
     set_place_cards();
@@ -53,19 +54,34 @@
 
   function generate_plate () {
     var $bg_plate = $("#bg_plate");
-    for(var i = 1; i <= 100; i++){
+    for(var i = 1; i <= 30; i++){
       $bg_plate.append("<div id='plate_" + i + "'class='plate_base'/>");
+      if (i % 4 === 1) {
+      $("#plate_" + i).addClass("plate_01");
+      }
+      else if (i % 4 === 2) {
+        $("#plate_" + i).addClass("plate_02");
+      }
+      else if (i % 4 === 3) {
+        $("#plate_" + i).addClass("plate_03");
+      }
+      else {
+        $("#plate_" + i).addClass("plate_04");
+      }
     }
-		// jqueryのidセレクタは、文字列べた書きしないと、addClassしたときにCSS3のプロパティを反映してくれない。
-		// $("#plate_" + i)とか書けない。変数でもダメ。どうしてくれよう・・・・・・
-    $("#plate_1").addClass("plate_01");
-    $("#plate_2").addClass("plate_02");
-    $("#plate_3").addClass("plate_03");
-    $("#plate_4").addClass("plate_04");
   }
 	
 	function arrange_plate () {
-		
+		var windowInnerTop = window.innerHeight/2;
+    var windowInnerLeft = window.innerWidth/2;
+    for (var i = 1; i <= 30; i++) {
+			var top = (windowInnerTop - $("#plate_" + i).height()/2) * Math.random()*2;
+			var left = (windowInnerLeft - $("#plate_" + i).width()/2) * Math.random()*2;
+		  $("#plate_" + i).css("top", String(top) + "px");
+      $("#plate_" + i).css("left", String(left) + "px");
+			var horizontalRatio = (left - windowInnerLeft)/windowInnerLeft;
+      $("#plate_" + i).css('-webkit-transform','matrix(0.8, '+ horizontalRatio +', 0, 0.8, 0, 0)');
+	  }
 	}
 
   function show_next () {
